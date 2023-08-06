98b97798149395feb15ffa1f2f036e8ed18dca5a
anonymous <anonymous@anonymo.us>
2023-08-06 05:48:49 +0000

    test exam

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
-import { Context, Schema } from 'koishi'
+declare module 'koishi' {
+  interface User {
+    exam_ongoing: number,
+    exam_passed: number[]
+  }
+}
+
+import { Context, Session, Schema } from 'koishi'
 
 export const name = 'choice-rating'
 
@@ -6,6 +13,37 @@ export interface Config {}
 
 export const Config: Schema<Config> = Schema.object({})
 
+function testArg(arg: number) {
+  if (arg < 2) {
+
+  }
+}
+
 export function apply(ctx: Context) {
-  // write your plugin here
+  ctx.model.extend('user', {
+    exam_ongoing: "unsigned",
+    exam_passed: "list",
+  })
+
+  ctx.command('exam [arg:number]')
+    .userFields(['exam_ongoing'])
+    .action(({session}, arg) => {
+      if(arg) {
+        session.user.exam_ongoing = arg;
+      } else {
+        return 'exam list'
+      }
+    })
+
+  ctx.command('show')
+    .userFields(['exam_ongoing'])
+    .action(({session}) => JSON.stringify(session.user.exam_ongoing))
+
+  ctx.command('submit <answer:text>')
+    .userFields(['exam_ongoing'])
+    .action(({session}, answer) => checkAnswer(answer, session.user.exam_ongoing))
+}
+
+function checkAnswer(answer: string, exam: number) {
+  return answer
 }
